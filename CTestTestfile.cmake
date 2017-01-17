e174e776a86d612a2d479557b069682b84f6aff6
james-isbister <isbisterjb@gmail.com>
2017-01-17 16:00:46 +0000

    Misc Test cmake files removed

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/james/Documents/JISpikeExperimentsRepository/Set1/Spike
-# Build directory: /home/james/Documents/JISpikeExperimentsRepository/Set1/Spike
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(Spike)
-subdirs(Tests)
